68498e956e9dcb3520d54aed927213e89d54e78a
Aikeilol <garena3307@mail.ru>
2022-01-17 14:30:40 +0300

    table render and label

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 'use strict'
 
+const container = document.querySelector('.container');
 const nameContainer = document.querySelector('.main__ul');
-const users = ['Иван Ургант', 'Семен Слепаков'];
+const tbody = document.querySelector('tbody');
+const columnSelect = document.querySelector('.main__row select');
+const users = [];
+const fields = [];
 
 showUserNames(users)
 getUsers()
-
 // function showUserNames(users) {
 //   nameContainer.innerHTML = '';
 //   for (const user of users) {
@@ -14,7 +17,7 @@ getUsers()
 // }
 
 function showUserNames(names) {
-  nameContainer.innerHTML = names.map(name => `<li class="main__li">${name}</li>` ).join('');
+  nameContainer.innerHTML = names.map(name => `<li class="main__li">${name}</li>`).join('');
 }
 
 function getUsers() {
@@ -22,22 +25,67 @@ function getUsers() {
     .then(parseJSON).then(takeUserNames).then(showUserNames)
 }
 
-function parseJSON(response){
+function getUsers() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(parseJSON).then(users => {
+      fillSelect(getFields(users[0]))
+      showUserInfo(users);
+      showUserNames(takeUserNames(users));
+      container.hidden = false;
+    })
+}
+
+function parseJSON(response) {
   return response.json();
 }
 
-function extractName(obj){
+function extractName(obj) {
   return obj.name;
 }
 
-function mapWith(cb){
-  return function(arr){
+function mapWith(cb) {
+  return function (arr) {
     return arr.map(cb);
   }
 }
 
 // takeUserNames = mapWith(extractName);
 
-function takeUserNames(users){
+function takeUserNames(users) {
   return mapWith(extractName)(users)
+}
+
+function showUserInfo(users) {
+  tbody.innerHTML = users.map(user => {
+    return `
+      <tr>
+        <td>${user.name}</td>
+        <td>${user.email}</td>
+        <td>${user.username}</td>
+      </tr>
+    `
+  }).join('');
+}
+
+function fillSelect(labels) {
+  columnSelect.innerHTML = labels.map(label => `<option>${label}</option>`).join('');
+}
+
+function getFields(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] != 'object') {
+      fields.push(key);
+    } else {
+      for (const key2 in obj[key]) {
+        if (typeof obj[key][key2] != 'object') {
+          fields.push(key + '.' + key2);
+        } else {
+          for (const key3 in obj[key][key2]) {
+            fields.push(key + '.' + key2 + '.' + key3);
+          }
+        }
+      }
+    }
+  }
+  return fields;
 }
